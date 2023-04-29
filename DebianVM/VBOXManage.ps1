4a84f87beb142e93d90b2f9d6734242d7075249a
mappel-code <matthias.appelmans@student.hogent.be>
2023-04-29 10:33:43 +0200

    DebianVM

---
+++ DebianVM/VBOXManage.ps1
@@ -0,0 +1,43 @@
+############################################
+## Matthias Appelmans / Pieter De Koninck ##
+############################################
+##  CYBERSECURITY AUTOMATED INSTALLATION  ##
+############################################
+
+## VBOX Manage ##
+
+
+function create_vm {
+
+    $vmPath = "C:\Users\$($env:UserName)\VirtualBox VMs\"
+    $vmName = "DebianBad"
+    $hdSizeMb = 4096
+    $isoPath = "C:\ISO Files"
+   
+    vboxmanage createvm --name $vmName --basefolder=$vmPath --register 2> $null
+    VBoxManage createmedium --filename $vmPath\$($vmName).vdi --size $hdSizeMb 2> $null
+
+    VBoxManage storagectl    $vmName --name       'SATA Controller' --add sata --controller IntelAhci
+    VBoxManage storageattach $vmName --storagectl 'SATA Controller' --port 0 --device 0 --type hdd --medium  $vmPath/$vmName.vdi
+
+    VBoxManage storagectl    $vmName --name       'IDE Controller' --add ide
+    VBoxManage storageattach $vmName --storagectl 'IDE Controller' --port 0 --device 0 --type dvddrive --medium $isoPath
+
+    VBoxManage modifyvm $vmName --ioapic on
+
+    VBoxManage modifyvm $vmName --boot1 dvd --boot2 disk --boot3 none --boot4 none
+
+    VBoxManage modifyvm $vmName --memory 4096 --vram 1048
+
+    BoxManage modifyvm $vmName --cpus 4
+
+    VBoxManage modifyvm $vmName --nic1 bridged
+
+    VBoxManage modifyvm $vmName --clipboard-mode bidirectional
+
+    VBoxManage modifyvm $vmName --graphicscontroller vboxsvga
+
+}
+
+
+
