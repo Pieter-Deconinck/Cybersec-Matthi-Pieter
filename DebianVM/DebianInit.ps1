55b77a41bb0478860d100795a6ac9bd16563ecfb
mappel-code <matthias.appelmans@student.hogent.be>
2023-04-30 10:41:57 +0200

    Added working debian init

---
+++ DebianVM/DebianInit.ps1
@@ -0,0 +1,18 @@
+############################################
+## Matthias Appelmans / Pieter Deconinck ##
+############################################
+##  CYBERSECURITY AUTOMATED INSTALLATION  ##
+############################################
+
+VBoxManage createvm --name VulDebian --ostype Debian --register
+VBoxManage modifyvm VulDebian --memory 4096 --cpus 4
+VBoxManage modifyvm VulDebian --ioapic on
+VboxManage modifyvm VulDebian --vram=512
+VBoxManage modifyvm VulDebian --nic1 bridged --bridgeadapter1 "$(wmic nic where NetEnabled=true get DeviceID | findstr /r /c:\"[0-9]*\")"
+
+VBoxManage createhd --filename VirtualBox\ VMs/VulDebian/VulDebian.vdi --size 25000 --format VDI
+VBoxManage storagectl VulDebian --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach VulDebian --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium VirtualBox\ VMs/VulDebian/VulDebian.vdi 
+VBoxManage storagectl VulDebian --name "IDE Controller" --add ide --controller PIIX4
+VBoxManage storageattach VulDebian --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "C:\ISO Files\debian-11.6.0-amd64-netinst.iso"
+VBoxHeadless --startvm VulDebian
