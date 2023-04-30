c50dd609485a96196782dee4012d6c47be9224be
mappel-code <matthias.appelmans@student.hogent.be>
2023-04-30 15:12:19 +0200

    Pieter is heet

--- VMs/DebianInit.ps1
+++ VMs/DebianInit.ps1
@@ -9,9 +9,15 @@
 VBoxManage createvm --name VulDebian --ostype Debian --register
 VBoxManage modifyvm VulDebian --memory 4096 --cpus 4
 VBoxManage modifyvm VulDebian --ioapic on
-VboxManage modifyvm VulDebian --vram=512
+VboxManage modifyvm VulDebian --vram=512$
+
+# Normaal wordt de juiste adapter automatisch gekozen.
+
 VBoxManage modifyvm VulDebian --nic1 bridged --bridgeadapter1 "$(wmic nic where NetEnabled=true get DeviceID | findstr /r /c:\"[0-9]*\")"
 VBoxManage createhd --filename VirtualBox\ VMs/VulDebian/VulDebian.vdi --size 25000 --format VDI
 VBoxManage storagectl VulDebian --name "SATA Controller" --add sata --controller IntelAhci
+
+# Pas zeker je pad aan naar de VDI file indien nodig.
+
 VBoxManage storageattach VulDebian --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\VDI Files\Debian\Debian 11 (32bit).vdi"
 VBoxManage startvm VulDebian --type gui
--- VMs/EvilKali.ps1
+++ VMs/EvilKali.ps1
@@ -10,8 +10,14 @@ VBoxManage createvm --name EvilKali --ostype KaliLinux --register
 VBoxManage modifyvm EvilKali --memory 4096 --cpus 4
 VBoxManage modifyvm EvilKali --ioapic on
 VboxManage modifyvm EvilKali --vram=512
+
+# Normaal wordt de juiste adapter automatisch gekozen.
+
 VBoxManage modifyvm EvilKali --nic1 bridged --bridgeadapter1 "$(wmic nic where NetEnabled=true get DeviceID | findstr /r /c:\"[0-9]*\")"
 VBoxManage createhd --filename VirtualBox\ VMs/EvilKali/EvilKali.vdi --size 25000 --format VDI
 VBoxManage storagectl EvilKali --name "SATA Controller" --add sata --controller IntelAhci
+
+# Pas zeker je pad aan naar de VDI file indien nodig
+
 VBoxManage storageattach EvilKali --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\VDI Files\Kali\Kali Linux 2022.3 (32bit).vdi"
 VBoxManage startvm EvilKali --type gui
