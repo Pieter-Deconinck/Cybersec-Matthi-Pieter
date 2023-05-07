8a19c3f4be15525b59bdcc385f7e1f582d5e9ea8
mappel-code <matthias.appelmans@student.hogent.be>
2023-05-07 13:44:36 +0200

    updates

--- Bash scripts/debian.sh
+++ Bash scripts/debian.sh
@@ -44,19 +44,9 @@ sudo chmod -R 755 /var/www/html/wordpress
 
 # Create MySQL database and user for WordPress
 sudo mysql -u root -p -e "CREATE DATABASE wordpress;"
-sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'finalfantasy';"
+sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'Matthias'@'localhost' IDENTIFIED BY 'finalfantasy';"
 
 # Configure WordPress
-sudo cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-sudo sed -i "s/database_name_here/wordpress/" /var/www/html/wordpress/wp-config.php
-sudo sed -i "s/username_here/wordpressuser/" /var/www/html/wordpress/wp-config.php
-sudo sed -i "s/password_here/finalfantasy/" /var/www/html/wordpress/wp-config.php
+
 
 # Update Apache configuration
-sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/wordpress.conf
-sudo sed -i "s|DocumentRoot /var/www/html|DocumentRoot /var/www/html/wordpress|" /etc/apache2/sites-available/wordpress.conf
-sudo sed -i "s/ServerAdmin webmaster@localhost/ServerAdmin admin@example.com/" /etc/apache2/sites-available/wordpress.conf
-sudo sed -i "s|<Directory /var/www/html>|<Directory /var/www/html/wordpress/>|" /etc/apache2/sites-available/wordpress.conf
-sudo a2ensite wordpress.conf
-sudo a2enmod rewrite
-sudo systemctl restart apache2
---
+++ VMs/DebianV.ps1
@@ -0,0 +1,27 @@
+############################################
+## Matthias Appelmans / Pieter Deconinck ##
+############################################
+##  CYBERSECURITY AUTOMATED INSTALLATION  ##
+############################################
+
+## MACHINE NAME =  DebianV
+
+$VM_NAME = "DebianV"
+$VM_HD_PATH = "C:\VDI Files\Debian\Debian 11 (32bit).vdi"
+VBoxManage startvm $VM_NAME --type gui
+
+VBoxManage createvm --name $VM_NAME --ostype Debian --register
+
+vboxmanage createmedium disk --filename "C:\Users\matth\VirtualBox VMs\DebianV\DebianV" --size 20480 --format VDI
+
+
+VBoxManage storagectl ${VM_NAME} --name "SATA Controller" --add sata --controller IntelAHCI
+
+VBoxManage storageattach ${VM_NAME} --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium ${VM_HD_PATH}
+
+vboxmanage modifyvm $VM_NAME --ioapic on
+vboxmanage modifyvm $VM_NAME --memory 4096 --vram 64
+vboxmanage modifyvm $VM_NAME --nic1 nat
+VBoxManage modifyvm $VM_NAME --nic2 intnet
+
+VBoxManage startvm VulDebian --type gui
