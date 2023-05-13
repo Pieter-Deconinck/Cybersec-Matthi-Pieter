4cbd4b9815a4f04f592c7f6822338d30ed9f674a
mappel-code <matthias.appelmans@student.hogent.be>
2023-05-13 11:12:32 +0200

    updates

--- Bash scripts/debian.sh
+++ Bash scripts/debian.sh
@@ -46,7 +46,87 @@ sudo chmod -R 755 /var/www/html/wordpress
 sudo mysql -u root -p -e "CREATE DATABASE wordpress;"
 sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'Matthias'@'localhost' IDENTIFIED BY 'finalfantasy';"
 
-# Configure WordPress
+# Configure/install WordPress
+
+sudo apt install wordpress curl mariadb -y
 
 
-# Update Apache configuration
+#Mariadb secure install
+
+sudo mysql_secure_installation
+
+# create virtual host for wordpress
+
+
+APACHE_LOG_DIR="/var/log/apache2"
+
+cat <<EOF > /etc/apache2/sites-available/wp.conf
+<VirtualHost *:80>
+            ServerName localhost
+    DocumentRoot /usr/share/wordpress
+    Alias /wp-content /var/lib/wordpress/wp-content
+    <Directory /usr/share/wordpress>
+        Options FollowSymLinks
+        AllowOverride Limit Options FileInfo
+        DirectoryIndex index.php
+        Require all granted
+    </Directory>
+    <Directory /var/lib/wordpress/wp-content>
+        Options FollowSymLinks
+        Require all granted
+    </Directory>
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+</VirtualHost>
+EOF
+
+# disable default virtual host and enable the site
+
+sudo a2dissite 000-default
+sudo a2ensite wp
+
+
+# Reload webserver
+
+sudo service apache2 reload
+
+# WordPress searches in the above order and uses the first configuration file it can find. The domain name is taken 
+#from the HTTP-Request of your browser. That way you may be able to define different configuration files for different domains you are hosting.
+
+sudo bash -c 'cat > /etc/wordpress/config-myblog.example.com.php 
+<<EOF
+<?php
+define("DB_NAME", "wordpress");
+define("DB_USER", "wordpress");
+define("DB_PASSWORD", "Matthias");
+define("DB_HOST", "localhost");
+define("WP_CONTENT_DIR", "/var/lib/wordpress/wp-content");
+?>
+EOF'
+
+
+# perms
+
+sudo chown www-data:www-data /etc/wordpress/config-myblog.example.com.php
+sudo chmod 640 /etc/wordpress/config-myblog.example.com.php
+
+
+# Create a file to hold the database creation instructions
+
+sudo touch ~/wp.sql
+
+
+
+# Add the following lines to the file 
+
+
+cat > ~/wp.sql <<EOF
+CREATE DATABASE wordpress;
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+EOF
+
+
+
+cat ~/wp.sql | mysql --defaults-extra-file=/etc/mysql/debian.cnf
