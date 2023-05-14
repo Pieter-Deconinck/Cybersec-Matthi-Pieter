f2d2ea18629d4ab830e3db0c91d1d5cfa1ea25c6
mappel-code <matthias.appelmans@student.hogent.be>
2023-05-14 17:49:12 +0200

    x

--- Bash scripts/kali.sh
+++ Bash scripts/kali.sh
@@ -10,7 +10,3 @@
 
 sudo ip addr add 192.168.1.61/24 dev enp0s3
 
-# Delete the 10.0.2.15 address if it exists
-if sudo ip addr show enp0s3 | grep "10.0.2.15" > /dev/null; then
-  sudo ip addr del 10.0.2.15/24 dev enp0s3
-fi
