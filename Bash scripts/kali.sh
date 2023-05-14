5017e921e76f281d9b484a7a8fd221d7c605bfa9
mappel-code <matthias.appelmans@student.hogent.be>
2023-05-14 20:44:48 +0200

    x

--- Bash scripts/kali.sh
+++ Bash scripts/kali.sh
@@ -8,5 +8,10 @@
 ############################################
 
 
-sudo ip addr add 192.168.1.61/24 dev enp0s3
 
+#!/bin/bash
+echo "auto eth1" | sudo tee -a /etc/network/interfaces
+echo "iface eth1 inet static" | sudo tee -a /etc/network/interfaces
+echo "address 192.168.1.61" | sudo tee -a /etc/network/interfaces
+echo "netmask 255.255.255.0" | sudo tee -a /etc/network/interfaces
+sudo ifup eth1
