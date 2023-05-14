84555acded13c50e20996e273215e3fbb5d275f7
mappel-code <matthias.appelmans@student.hogent.be>
2023-05-14 20:45:02 +0200

    x

--- Bash scripts/kali.sh
+++ Bash scripts/kali.sh
@@ -8,8 +8,6 @@
 ############################################
 
 
-
-#!/bin/bash
 echo "auto eth1" | sudo tee -a /etc/network/interfaces
 echo "iface eth1 inet static" | sudo tee -a /etc/network/interfaces
 echo "address 192.168.1.61" | sudo tee -a /etc/network/interfaces
