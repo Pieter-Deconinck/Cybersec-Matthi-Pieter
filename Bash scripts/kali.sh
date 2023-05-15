12d8c363fb1debec8e01afbe5ce8fe8f4a0eef65
mappel-code <matthias.appelmans@student.hogent.be>
2023-05-15 09:07:01 +0200

    x

--- Bash scripts/kali.sh
+++ Bash scripts/kali.sh
@@ -13,3 +13,4 @@ echo "iface eth1 inet static" | sudo tee -a /etc/network/interfaces
 echo "address 192.168.1.61" | sudo tee -a /etc/network/interfaces
 echo "netmask 255.255.255.0" | sudo tee -a /etc/network/interfaces
 sudo ifup eth1
+sudo systemctl restart NetworkManager
